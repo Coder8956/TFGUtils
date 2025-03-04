9f232ed3a5b49bcd6faf9a41ad88ff57119572df
yanhao.home <yanhao.home@gmail.com>
2025-03-04 17:04:53 +0800

    [modify]  整理代码命名空间

--- TFGUtilsProject/Assets/TFGUtils/Runtime/Component/Gizmos/TFGR_GizDrawGOCoordinate.cs
+++ TFGUtilsProject/Assets/TFGUtils/Runtime/Component/Gizmos/TFGR_GizDrawGOCoordinate.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
 
-namespace TFGUtils.EditorUtils.TFGGizmo
+namespace TFGUtils.Runtime.Component.Gizmos
 {
-    namespace TFGEditorUtils.TFGGizmo
+    public class TFGR_GizDrawGOCoordinate : MonoBehaviour
     {
-        public class TFGR_GizDrawGOCoordinate : MonoBehaviour
-        {
-            [SerializeField] private int m_scaleAxis = 20;
+        [SerializeField] private int m_scaleAxis = 20;
 
-            private void OnDrawGizmos()
-            {
-                Debug.DrawRay(transform.position, transform.forward * m_scaleAxis, Color.blue);
-                Debug.DrawRay(transform.position, transform.up * m_scaleAxis, Color.green);
-                Debug.DrawRay(transform.position, transform.right * m_scaleAxis, Color.red);
-            }
+        private void OnDrawGizmos()
+        {
+            Debug.DrawRay(transform.position, transform.forward * m_scaleAxis, Color.blue);
+            Debug.DrawRay(transform.position, transform.up * m_scaleAxis, Color.green);
+            Debug.DrawRay(transform.position, transform.right * m_scaleAxis, Color.red);
         }
     }
 }
